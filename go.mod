1c2ea667d6f6ebcce796ba6f11167f3c8761942f
piotrostr <piotr.ostrowski@aol.com>
2022-06-14 21:45:36 +0200

    add some of the buying functionality and test buy

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/piotrostr/gobuy
 
 go 1.18
 
-require github.com/kardianos/service v1.2.1
+require (
+	github.com/adshao/go-binance/v2 v2.3.5
+	github.com/joho/godotenv v1.4.0
+	github.com/kardianos/service v1.2.1
+)
 
-require golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+)
--- main.go
+++ main.go
@@ -1,27 +1,74 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"os"
 
+	binance "github.com/adshao/go-binance/v2"
+	"github.com/joho/godotenv"
 	"github.com/kardianos/service"
 )
 
-type Client struct{}
+var ctx = context.Background()
 
 type Buyer struct {
-	quantity float64
+	quantity string
 	interval int
-	client   *Client
+	client   *binance.Client
+}
+
+func GetBuyer(quantity string, interval int) (*Buyer, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		return nil, errors.New("API_KEY not set")
+	}
+	secretKey := os.Getenv("SECRET_KEY")
+	if secretKey == "" {
+		return nil, errors.New("SECRET_KEY not set")
+	}
+	client := binance.NewClient(apiKey, secretKey)
+	buyer := &Buyer{
+		quantity: quantity,
+		interval: interval,
+		client:   client,
+	}
+	return buyer, nil
+}
+
+func (b *Buyer) Buy() (*binance.CreateOrderResponse, error) {
+	order := b.client.NewCreateOrderService()
+	order.Symbol("ETHUSDT").Side("BUY").Type("MARKET")
+	res, err := order.Quantity(b.quantity).Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (b *Buyer) TestBuy() error {
+	order := b.client.NewCreateOrderService()
+	order.Symbol("ETHUSDT").Side("BUY").Type("MARKET")
+	err := order.Quantity(b.quantity).Test(ctx)
+	if err != nil {
+		return err
+	}
+	return err
 }
 
 func (b *Buyer) Start(s service.Service) error {
-	fmt.Println("asdf")
+	println("starting")
 	return nil
 }
 
 func (b *Buyer) Stop(s service.Service) error {
-	fmt.Println("fdsa")
+	println("shutting down")
 	return nil
 }
 
@@ -31,15 +78,17 @@ func main() {
 		DisplayName: "gobuy",
 		Description: "Buy X ETH every N time.",
 	}
-	quantity := flag.Float64("qty", 0.01, "Quantity of ETH to buy")
+	quantity := flag.String("qty", "0.01", "Quantity of ETH to buy")
 	interval := flag.Int("interval", 1, "Interval")
-	buyer := &Buyer{
-		quantity: *quantity,
-		interval: *interval,
+	flag.Parse()
+	buyer, err := GetBuyer(*quantity, *interval)
+	if err != nil {
+		println(err.Error())
+		return
 	}
 	s, err := service.New(buyer, config)
 	if err != nil {
-		println(err)
+		println(err.Error())
 		return
 	}
 	msg := "Starting gobuy service, will buy %d ETH every %s"
